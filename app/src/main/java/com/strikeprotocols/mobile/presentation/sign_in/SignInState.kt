32676413b29732cba376bec789fbc09b33604b88
Zakarie Ortiz <ZOrtiz42@outlook.com>
2022-03-01 14:17:25 -0500

    Setup AuthFlow for first time login

--- app/src/main/java/com/strikeprotocols/mobile/presentation/sign_in/SignInState.kt
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/sign_in/SignInState.kt
@@ -11,12 +11,16 @@ data class SignInState(
     val password: String = "",
     val emailErrorEnabled: Boolean = false,
     val passwordErrorEnabled: Boolean = false,
+
+    //Async Data
     val loginResult: Resource<String> = Resource.Uninitialized,
-    val verifyResult: Resource<VerifyUser> = Resource.Uninitialized,
+    val verifyUserResult: Resource<VerifyUser> = Resource.Uninitialized,
     val walletSignersResult: Resource<WalletSigners> = Resource.Uninitialized,
     val addWalletSignerResult: Resource<WalletSigner> = Resource.Uninitialized
 ) {
     val signInButtonEnabled = email.isNotEmpty() && password.isNotEmpty()
+
+    val userHasPublicKey = verifyUserResult is Resource.Success && !verifyUserResult.data?.publicKeys.isNullOrEmpty()
 
     fun emailValid() = Patterns.EMAIL_ADDRESS.matcher(email).matches()
     fun passwordValid() = password.isNotEmpty()
