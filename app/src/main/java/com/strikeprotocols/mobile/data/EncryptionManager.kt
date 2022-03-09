a1dc55206bc39d4ede513096fe0fce3146b7d58d
samiamharris <samharris512@gmail.com>
2022-03-09 08:51:09 -0500

    Setup verify and signing methods to work with Private/Public key strings. Split password into iv and key when encrypting/decrypting with AES

--- app/src/main/java/com/strikeprotocols/mobile/data/EncryptionManager.kt
+++ app/src/main/java/com/strikeprotocols/mobile/data/EncryptionManager.kt
@@ -1,25 +1,24 @@
 package com.strikeprotocols.mobile.data
 
-
 import com.strikeprotocols.mobile.common.BaseWrapper
-import com.strikeprotocols.mobile.data.EncryptionManagerImpl.Companion.AUTH_TAG_LENGTH
+import com.strikeprotocols.mobile.data.EncryptionManagerImpl.Companion.IV_AND_KEY_COMBINED_LENGTH
 import com.strikeprotocols.mobile.data.EncryptionManagerImpl.Companion.IV_LENGTH
 import com.strikeprotocols.mobile.data.EncryptionManagerImpl.Companion.NO_OFFSET_INDEX
-import org.bouncycastle.crypto.AsymmetricCipherKeyPair
 import org.bouncycastle.crypto.AsymmetricCipherKeyPairGenerator
 import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator
-import org.bouncycastle.crypto.params.AsymmetricKeyParameter
 import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters
+import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters
 import org.bouncycastle.crypto.signers.Ed25519Signer
 import java.security.SecureRandom
 import javax.crypto.Cipher
-import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 interface EncryptionManager {
-    fun createKeyPair(): AsymmetricCipherKeyPair
-    fun signData(data: String, privateKey: AsymmetricKeyParameter): ByteArray
-    fun verifyData(data: String, signature: ByteArray, publicKey: AsymmetricKeyParameter): Boolean
+    fun createKeyPair(): StrikeKeyPair
+    fun signData(data: String, privateKey: String): ByteArray
+    fun verifyData(data: String, signature: ByteArray, publicKey: String): Boolean
     fun encrypt(message: String, generatedPassword: ByteArray): String
     fun decrypt(encryptedMessage: String, generatedPassword: ByteArray): String
     fun generatePassword(): ByteArray
@@ -28,30 +27,38 @@ interface EncryptionManager {
 class EncryptionManagerImpl : EncryptionManager {
 
     //region interface methods
-    override fun signData(data: String, privateKey: AsymmetricKeyParameter): ByteArray {
+    override fun signData(data: String, privateKey: String): ByteArray {
         val decodedData = BaseWrapper.decodeFromUTF8(data)
+        val privateKeyByteArray = BaseWrapper.decode(privateKey)
+        val privateKeyParam = Ed25519PrivateKeyParameters(privateKeyByteArray.inputStream())
 
         val signer = Ed25519Signer()
-        signer.init(true, privateKey)
+        signer.init(true, privateKeyParam)
         signer.update(decodedData, NO_OFFSET_INDEX, decodedData.size)
 
         return signer.generateSignature()
     }
 
-    override fun verifyData(data: String, signature: ByteArray, publicKey: AsymmetricKeyParameter): Boolean {
+    override fun verifyData(data: String, signature: ByteArray, publicKey: String): Boolean {
+        val publicByteArray = BaseWrapper.decode(publicKey)
+        val publicKeyParameter = Ed25519PublicKeyParameters(publicByteArray.inputStream())
         val decodedData = BaseWrapper.decodeFromUTF8(data)
 
         val verifier = Ed25519Signer()
-        verifier.init(false, publicKey)
+        verifier.init(false, publicKeyParameter)
         verifier.update(decodedData, NO_OFFSET_INDEX, decodedData.size)
 
         return verifier.verifySignature(signature)
     }
 
-    override fun createKeyPair(): AsymmetricCipherKeyPair {
+    override fun createKeyPair(): StrikeKeyPair {
         val keyPairGenerator: AsymmetricCipherKeyPairGenerator = Ed25519KeyPairGenerator()
         keyPairGenerator.init(Ed25519KeyGenerationParameters(SecureRandom()))
-        return keyPairGenerator.generateKeyPair()
+        val keyPair = keyPairGenerator.generateKeyPair()
+        val privateKey = (keyPair.private as Ed25519PrivateKeyParameters).encoded
+        val publicKey = (keyPair.public as Ed25519PublicKeyParameters).encoded
+
+        return StrikeKeyPair(privateKey = privateKey, publicKey = publicKey)
     }
 
     override fun encrypt(message: String, generatedPassword: ByteArray): String {
@@ -67,27 +74,53 @@ class EncryptionManagerImpl : EncryptionManager {
     }
 
     override fun generatePassword(): ByteArray =
-        SecureRandom().generateSeed(Companion.PASSWORD_BYTE_LENGTH)
+        SecureRandom().generateSeed(IV_AND_KEY_COMBINED_LENGTH)
     //endregion
 
     //region helper methods
     private fun getCipher(opMode: Int, password: ByteArray): Cipher {
-        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        val secretKeySpec = SecretKeySpec(password, "AES")
 
-        //Another option could be:
-        //cipher.init(opMode, secretKeySpec, IvParameterSpec(ByteArray(IV_LENGTH)))
-        cipher.init(opMode, secretKeySpec, GCMParameterSpec(AUTH_TAG_LENGTH, ByteArray(IV_LENGTH)))
+        if(password.size != IV_AND_KEY_COMBINED_LENGTH) {
+            throw IllegalStateException("iv and key are not the correct size")
+        }
+
+        val ivParameter = password.slice(0.until(IV_LENGTH)).toByteArray()
+        val key = password.slice(IV_LENGTH.until(password.size)).toByteArray()
+
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val secretKeySpec = SecretKeySpec(key, "AES")
+
+        cipher.init(opMode, secretKeySpec, IvParameterSpec(ivParameter))
         return cipher
     }
     //endregion
 
     //region companion
     object Companion {
-        const val PASSWORD_BYTE_LENGTH = 32
+        private const val PASSWORD_BYTE_LENGTH = 32
         const val IV_LENGTH = 12
-        const val AUTH_TAG_LENGTH = 128
+        const val IV_AND_KEY_COMBINED_LENGTH = PASSWORD_BYTE_LENGTH + IV_LENGTH
         const val NO_OFFSET_INDEX = 0
     }
     //endregion
+}
+
+data class StrikeKeyPair(val privateKey: ByteArray, val publicKey: ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as StrikeKeyPair
+
+        if (!privateKey.contentEquals(other.privateKey)) return false
+        if (!publicKey.contentEquals(other.publicKey)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = privateKey.contentHashCode()
+        result = 31 * result + publicKey.contentHashCode()
+        return result
+    }
 }
