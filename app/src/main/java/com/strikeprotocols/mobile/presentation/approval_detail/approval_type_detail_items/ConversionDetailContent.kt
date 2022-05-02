a3a807f39e85bd7680c46726f06a29a2facd4a46
samiamharris <samharris512@gmail.com>
2022-05-02 11:44:00 -0400

    Setup conversion and withdrawal detail UI

---
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/ConversionDetailContent.kt
@@ -0,0 +1,79 @@
+package com.strikeprotocols.mobile.presentation.approval_detail.approval_type_detail_items
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.strikeprotocols.mobile.R
+import com.strikeprotocols.mobile.common.formatISO8601IntoDisplayText
+import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
+import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.TransferConversionContent
+import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.TransferConversionLabelData
+import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.getMainValueText
+import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.getUSDEquivalentText
+import com.strikeprotocols.mobile.ui.theme.DetailInfoDarkBackground
+import com.strikeprotocols.mobile.ui.theme.DetailInfoLightBackground
+import com.strikeprotocols.mobile.ui.theme.DividerGrey
+
+@Composable
+fun ConversionDetailContent(
+    conversionRequest: SolanaApprovalRequestType.ConversionRequest,
+    submitterEmail: String,
+    submitterDate: String,
+    approvalsNeeded: Int
+) {
+    val mainValue = conversionRequest.symbolAndAmountInfo.getMainValueText()
+    val usdEquivalent = conversionRequest.symbolAndAmountInfo.getUSDEquivalentText(
+        context = LocalContext.current,
+        hideSymbol = true
+    )
+    val fromAccount = conversionRequest.account.name
+    val toAccount = conversionRequest.destination.name
+
+    val labelData = TransferConversionLabelData(
+        showLabels = true,
+        label1 = stringResource(id = R.string.from),
+        label2 = stringResource(id = R.string.to),
+        subText = conversionRequest.destination.subName
+    )
+
+
+    Column(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .background(color = DetailInfoLightBackground),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TransferConversionContent(
+            mainValue = mainValue,
+            usdEquivalent = usdEquivalent,
+            fromText = fromAccount,
+            toText = toAccount,
+            transferConversionLabelData = labelData
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        ApprovalDispositionsRequired(approvalsNeeded = approvalsNeeded)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        ApprovalInfoRow(
+            backgroundColor = DetailInfoLightBackground,
+            title = stringResource(R.string.requested_by),
+            value = submitterEmail
+        )
+        Divider(modifier = Modifier.height(0.5.dp), color = DividerGrey)
+        ApprovalInfoRow(
+            backgroundColor = DetailInfoDarkBackground,
+            title = stringResource(R.string.requested_date),
+            value = submitterDate.formatISO8601IntoDisplayText(LocalContext.current)
+        )
+        Divider(modifier = Modifier.height(0.5.dp), color = DividerGrey)
+    }
+}
---
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/WithdrawalRequestDetailContent.kt
@@ -0,0 +1,82 @@
+package com.strikeprotocols.mobile.presentation.approval_detail.approval_type_detail_items
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.strikeprotocols.mobile.R
+import com.strikeprotocols.mobile.common.convertPublicKeyToDisplayText
+import com.strikeprotocols.mobile.common.formatISO8601IntoDisplayText
+import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
+import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.*
+import com.strikeprotocols.mobile.ui.theme.DetailInfoDarkBackground
+import com.strikeprotocols.mobile.ui.theme.DetailInfoLightBackground
+import com.strikeprotocols.mobile.ui.theme.DividerGrey
+
+@Composable
+fun WithdrawalRequestDetailContent(
+    withdrawalRequest: SolanaApprovalRequestType.WithdrawalRequest,
+    submitterEmail: String,
+    submitterDate: String,
+    approvalsNeeded: Int
+) {
+    val mainValue = withdrawalRequest.symbolAndAmountInfo.getMainValueText()
+    val usdEquivalent = withdrawalRequest.symbolAndAmountInfo.getUSDEquivalentText(
+        context = LocalContext.current,
+        hideSymbol = true
+    )
+    val fromAccount = withdrawalRequest.account.name
+    val toAccount = withdrawalRequest.destination.name
+
+    val labelData = TransferConversionLabelData(
+        showLabels = true,
+        label1 = stringResource(id = R.string.from),
+        label2 = stringResource(id = R.string.to),
+        subText = withdrawalRequest.destination.subName
+    )
+
+    Column(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .background(color = DetailInfoLightBackground),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TransferConversionContent(
+            mainValue = mainValue,
+            usdEquivalent = usdEquivalent,
+            fromText = fromAccount,
+            toText = toAccount,
+            transferConversionLabelData = labelData
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        ApprovalDispositionsRequired(approvalsNeeded = approvalsNeeded)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        ApprovalInfoRow(
+            backgroundColor = DetailInfoLightBackground,
+            title = stringResource(R.string.requested_by),
+            value = submitterEmail
+        )
+        Divider(modifier = Modifier.height(0.5.dp), color = DividerGrey)
+        ApprovalInfoRow(
+            backgroundColor = DetailInfoDarkBackground,
+            title = stringResource(R.string.requested_date),
+            value = submitterDate.formatISO8601IntoDisplayText(LocalContext.current)
+        )
+        Divider(modifier = Modifier.height(0.5.dp), color = DividerGrey)
+        val address = withdrawalRequest.destination.address
+        ApprovalInfoRow(
+            backgroundColor = DetailInfoLightBackground,
+            title = stringResource(R.string.address),
+            value = address.convertPublicKeyToDisplayText()
+        )
+    }
+}
