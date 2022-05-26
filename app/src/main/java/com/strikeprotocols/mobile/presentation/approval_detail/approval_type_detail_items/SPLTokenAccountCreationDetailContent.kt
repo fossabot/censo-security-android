29cf053365cfb70246297819e4bf809e40b41967
Zakarie Ortiz <ZOrtiz42@outlook.com>
2022-05-26 11:19:49 -0400

    Filled out the composables for the SPLToken row and detail UI

--- app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/SPLTokenAccountCreationDetailContent.kt
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/SPLTokenAccountCreationDetailContent.kt
@@ -1,16 +1,44 @@
 package com.strikeprotocols.mobile.presentation.approval_detail.approval_type_detail_items
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.strikeprotocols.mobile.R
 import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
 import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.getHeader
-import com.strikeprotocols.mobile.ui.theme.StrikeWhite
+import com.strikeprotocols.mobile.presentation.components.AccountChangeItem
+import com.strikeprotocols.mobile.ui.theme.AccountTextGrey
+import com.strikeprotocols.mobile.ui.theme.DetailInfoLightBackground
+import com.strikeprotocols.mobile.ui.theme.GreyText
 
 @Composable
 fun SPLTokenAccountCreationDetailContent(
     splTokenAccountCreation: SolanaApprovalRequestType.SPLTokenAccountCreation
 ) {
     val header = splTokenAccountCreation.getHeader(LocalContext.current)
-    Text(text = "Implement SPL Token Account Creation Detail UI", color = StrikeWhite)
+    val accountName = splTokenAccountCreation.payerBalanceAccount.name
+    val accountType = splTokenAccountCreation.payerBalanceAccount.accountType.name
+    val symbol = splTokenAccountCreation.tokenSymbolInfo.symbol
+
+    AccountChangeItem(header = header, title = accountName, subtitle = accountType, headerTopSpacing = 24)
+    Spacer(modifier = Modifier.height(24.dp))
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+            .background(color = DetailInfoLightBackground),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.symbol_title), color = GreyText, modifier = Modifier.padding(start = 12.dp, top = 8.dp, bottom = 8.dp))
+        if (symbol.isNotEmpty()) {
+            Text(text = symbol, color = AccountTextGrey, modifier = Modifier.padding(end = 12.dp))
+        }
+    }
 }
--- app/src/main/java/com/strikeprotocols/mobile/presentation/approvals/approval_type_row_items/SPLTokenAccountCreationRowContent.kt
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approvals/approval_type_row_items/SPLTokenAccountCreationRowContent.kt
@@ -1,15 +1,42 @@
 package com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
-import com.strikeprotocols.mobile.ui.theme.StrikeWhite
+import com.strikeprotocols.mobile.presentation.approvals.ApprovalContentHeader
+import com.strikeprotocols.mobile.presentation.components.StrikeTag
+import com.strikeprotocols.mobile.ui.theme.GreyText
+import com.strikeprotocols.mobile.ui.theme.SectionBlack
 
 @Composable
 fun SPLTokenAccountCreationRowContent(
     splTokenAccountCreation: SolanaApprovalRequestType.SPLTokenAccountCreation
 ) {
     val header = splTokenAccountCreation.getHeader(LocalContext.current)
-    Text(text = "Implement SPL Token Account Creation Row UI", color = StrikeWhite)
+    val accountName = splTokenAccountCreation.payerBalanceAccount.name
+    val symbol = splTokenAccountCreation.tokenSymbolInfo.symbol
+
+    ApprovalContentHeader(header = header, topSpacing = 24)
+    Spacer(modifier = Modifier.height(8.dp))
+    Text(symbol, color = GreyText, fontSize = 13.sp)
+    Spacer(modifier = Modifier.height(24.dp))
+
+    val tagPaddingValues = PaddingValues(top = 12.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
+    StrikeTag(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = SectionBlack),
+        text = accountName,
+        paddingValues = tagPaddingValues
+    )
 }
