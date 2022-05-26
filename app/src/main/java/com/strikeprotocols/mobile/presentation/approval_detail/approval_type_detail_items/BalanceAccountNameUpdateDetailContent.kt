f854185919dcc1b4520b52e4a54872c794703652
samiamharris <samharris512@gmail.com>
2022-05-26 09:44:51 -0400

    Setup balance account name update UI

--- app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/BalanceAccountNameUpdateDetailContent.kt
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approval_detail/approval_type_detail_items/BalanceAccountNameUpdateDetailContent.kt
@@ -1,16 +1,28 @@
 package com.strikeprotocols.mobile.presentation.approval_detail.approval_type_detail_items
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.strikeprotocols.mobile.R
 import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
+import com.strikeprotocols.mobile.presentation.approvals.ApprovalContentHeader
 import com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items.getHeader
-import com.strikeprotocols.mobile.ui.theme.StrikeWhite
+import com.strikeprotocols.mobile.presentation.components.StrikeTagLabeledRow
 
 @Composable
 fun BalanceAccountNameUpdateDetailContent(
     accountNameUpdate: SolanaApprovalRequestType.BalanceAccountNameUpdate
 ) {
     val header = accountNameUpdate.getHeader(LocalContext.current)
-    Text(text = "Implement Balance Account Name Update Detail UI", color = StrikeWhite)
+    val fromAccount = accountNameUpdate.accountInfo.name
+    val toAccount = accountNameUpdate.newAccountName
+
+    ApprovalContentHeader(header = header, topSpacing = 0, bottomSpacing = 36)
+    StrikeTagLabeledRow(
+        text1 = fromAccount,
+        text2 = toAccount,
+        label1 = stringResource(id = R.string.from),
+        label2 = stringResource(id = R.string.to),
+        arrowForward = true
+    )
 }
--- app/src/main/java/com/strikeprotocols/mobile/presentation/approvals/approval_type_row_items/BalanceAccountNameUpdateRowContent.kt
+++ app/src/main/java/com/strikeprotocols/mobile/presentation/approvals/approval_type_row_items/BalanceAccountNameUpdateRowContent.kt
@@ -1,15 +1,23 @@
 package com.strikeprotocols.mobile.presentation.approvals.approval_type_row_items
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.strikeprotocols.mobile.data.models.approval.SolanaApprovalRequestType
-import com.strikeprotocols.mobile.ui.theme.StrikeWhite
+import com.strikeprotocols.mobile.presentation.approvals.ApprovalContentHeader
+import com.strikeprotocols.mobile.presentation.components.StrikeTagRow
 
 @Composable
 fun BalanceAccountNameUpdateRowContent(
     accountNameUpdate: SolanaApprovalRequestType.BalanceAccountNameUpdate
 ) {
     val header = accountNameUpdate.getHeader(LocalContext.current)
-    Text(text = "Implement Balance Account Name Update Row UI", color = StrikeWhite)
+    val fromAccount = accountNameUpdate.accountInfo.name
+    val toAccount = accountNameUpdate.newAccountName
+
+    ApprovalContentHeader(header = header, topSpacing = 24, bottomSpacing = 24)
+    StrikeTagRow(
+        text1 = fromAccount,
+        text2 = toAccount,
+        arrowForward = true
+    )
 }
