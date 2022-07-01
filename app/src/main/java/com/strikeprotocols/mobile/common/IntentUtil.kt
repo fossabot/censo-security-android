44bddc23780a0d52a30f49cdd64c772056442f9a
SamIAmHarris <sam@groundcrewdev.com>
2022-07-01 11:26:12 -0400

    Add basic util work for intent to password managers

---
+++ app/src/main/java/com/strikeprotocols/mobile/common/IntentUtil.kt
@@ -0,0 +1,22 @@
+package com.strikeprotocols.mobile.common
+
+import android.content.Intent
+import android.content.pm.PackageManager
+
+const val LAST_PASS = "com.lastpass.lpandroid"
+const val ONE_PASSWORD = "com.agilebits.onepassword"
+
+fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
+    return try {
+        packageManager.getPackageInfo(packageName, 0)
+        true
+    } catch (e: PackageManager.NameNotFoundException) {
+        false
+    }
+}
+
+fun getIntentForPackage(packageManager: PackageManager, packageName: String) : Intent? {
+    return packageManager.getLaunchIntentForPackage(
+        packageName
+    )
+}
