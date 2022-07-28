882e83748aeb56c58044c517221f6126e42237db
SamIAmHarris <sam@groundcrewdev.com>
2022-07-27 21:27:02 -0400

    Initialize firebase after adding sha1 to google console

--- app/src/main/java/com/strikeprotocols/mobile/StrikeApplication.kt
+++ app/src/main/java/com/strikeprotocols/mobile/StrikeApplication.kt
@@ -1,6 +1,10 @@
 package com.strikeprotocols.mobile
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.raygun.raygun4android.RaygunClient
+import com.strikeprotocols.mobile.common.CrashReportingUtil
 import com.strikeprotocols.mobile.data.SharedPrefsHelper
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,5 +14,20 @@ class StrikeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         SharedPrefsHelper.setup(this)
+        setupFirebase()
+    }
+
+    private fun setupFirebase() {
+        try {
+            FirebaseApp.initializeApp(this)
+        } catch (e: Exception) {
+            RaygunClient.send(
+                e,
+                listOf(
+                    CrashReportingUtil.MANUALLY_REPORTED_TAG,
+                    CrashReportingUtil.PUSH_NOTIFICATION_TAG
+                )
+            )
+        }
     }
 }
