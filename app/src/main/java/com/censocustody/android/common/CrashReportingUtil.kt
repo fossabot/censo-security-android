30cc0bdfe0980b186bc2a1d763c7e9c5e1a7d366
SamIAmHarris <sam@groundcrewdev.com>
2022-12-20 10:32:35 -0500

    Show permission dialog to user if they need to give us permissions for push notifications.

--- app/src/main/java/com/censocustody/android/common/CrashReportingUtil.kt
+++ app/src/main/java/com/censocustody/android/common/CrashReportingUtil.kt
@@ -3,6 +3,7 @@ package com.censocustody.android.common
 object CrashReportingUtil {
     const val BROADCAST_RECEIVER_TAG = "BroadcastReceiver"
     const val PUSH_NOTIFICATION_TAG = "PushNotification"
+    const val PUSH_NOTIFICATION_PERMISSION_TAG = "PushNotificationPermission"
     const val MANUALLY_REPORTED_TAG = "ManualReport"
     const val FORCE_UPGRADE_TAG = "ForceUpgrade"
     const val JWT_TAG = "JWT"
