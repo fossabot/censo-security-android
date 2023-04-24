334cf46b606ef381eb7e6e30cfa9ef3e4cd657ec
SamIAmHarris <sam@groundcrewdev.com>
2023-04-24 08:47:03 -0400

    Add camera permission composable to QR code scanning screen.

--- app/src/main/java/com/censocustody/android/presentation/scan_qr/ScanQRScreen.kt
+++ app/src/main/java/com/censocustody/android/presentation/scan_qr/ScanQRScreen.kt
@@ -1,6 +1,10 @@
 package com.censocustody.android.presentation.scan_qr
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -18,6 +22,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -31,9 +36,12 @@ import com.censocustody.android.common.CensoButton
 import com.censocustody.android.common.Resource
 import com.censocustody.android.common.censoLog
 import com.censocustody.android.presentation.approvals.NavIconTopBar
+import com.censocustody.android.presentation.device_registration.Permission
 import com.censocustody.android.ui.theme.*
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import java.util.concurrent.Executors
 
+@OptIn(ExperimentalPermissionsApi::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @ExperimentalGetImage
 @Composable
@@ -67,149 +75,190 @@ fun ScanQRScreen(
         },
         content = { _ ->
 
-            Box(modifier = Modifier.fillMaxSize()) {
+            val context = LocalContext.current
 
-                val scanQRResult = state.scanQRCodeResult
-                val uploadWcUriResult = state.uploadWcUri
-
-                if (uploadWcUriResult is Resource.Success) {
-                    ScanQRBoxUI {
-                        Spacer(modifier = Modifier.height(36.dp))
+            Permission(
+                permission = Manifest.permission.CAMERA,
+                rationale = stringResource(R.string.explain_photo_permission),
+                permissionNotAvailableContent = {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 24.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         Text(
-                            modifier = Modifier.padding(horizontal = 8.dp),
-                            text = "DApp Connected Successfully!",
-                            textAlign = TextAlign.Center,
+                            stringResource(R.string.missing_camera_permission),
                             color = TextBlack,
-                            fontSize = 22.sp
+                            fontSize = 18.sp,
+                            textAlign = TextAlign.Center
                         )
-                        Spacer(modifier = Modifier.height(36.dp))
-                        CensoButton(onClick = viewModel::userFinished) {
+                        Spacer(modifier = Modifier.height(32.dp))
+                        CensoButton(
+                            onClick = {
+                                context.startActivity(
+                                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                        data = Uri.fromParts("package", context.packageName, null)
+                                    }
+                                )
+                            },
+                            contentPadding = PaddingValues(horizontal = 28.dp, vertical = 12.dp)
+                        ) {
                             Text(
-                                modifier = Modifier.padding(8.dp),
-                                text = "Done",
-                                fontSize = 20.sp,
-                                color = CensoWhite
+                                text = stringResource(R.string.open_settings),
+                                color = CensoWhite,
+                                fontSize = 18.sp,
+                                textAlign = TextAlign.Center
                             )
                         }
-                        Spacer(modifier = Modifier.height(36.dp))
-                    }
-                } else if (scanQRResult is Resource.Error || uploadWcUriResult is Resource.Error) {
-                    ScanQRBoxUI {
-                        Spacer(modifier = Modifier.height(36.dp))
-                        Text(
-                            modifier = Modifier.padding(horizontal = 8.dp),
-                            text = if (scanQRResult is Resource.Error) "Failed to scan QR Code" else "Failed to upload code to backend",
-                            textAlign = TextAlign.Center,
-                            color = TextBlack,
-                            fontSize = 22.sp
-                        )
-                        Spacer(modifier = Modifier.height(36.dp))
-                        CensoButton(onClick = viewModel::retryScan) {
-                            Text(
-                                modifier = Modifier.padding(8.dp),
-                                text = "Try Again",
-                                fontSize = 20.sp,
-                                color = CensoWhite
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(36.dp))
-                    }
-                } else if (scanQRResult is Resource.Uninitialized) {
-                    ScanQRBoxUI {
-                        Spacer(modifier = Modifier.height(36.dp))
-                        Text(
-                            modifier = Modifier.padding(horizontal = 8.dp),
-                            text = "Start Scanning...",
-                            textAlign = TextAlign.Center,
-                            color = TextBlack,
-                            fontSize = 22.sp
-                        )
-                        Spacer(modifier = Modifier.height(36.dp))
-                        CensoButton(onClick = viewModel::startScanning) {
-                            Text(
-                                modifier = Modifier.padding(8.dp),
-                                text = "Scan QR Code",
-                                fontSize = 20.sp,
-                                color = CensoWhite
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(36.dp))
-                    }
-                } else if (scanQRResult is Resource.Success) {
-                    ScanQRBoxUI {
-                        Spacer(modifier = Modifier.height(36.dp))
-                        Text(
-                            modifier = Modifier.padding(horizontal = 8.dp),
-                            text = "Found wallet, uploading data...",
-                            textAlign = TextAlign.Center,
-                            color = TextBlack,
-                            fontSize = 22.sp
-                        )
-                        Spacer(modifier = Modifier.height(36.dp))
-                        CircularProgressIndicator(
-                            modifier = Modifier.size(20.dp),
-                            color = ButtonRed,
-                            strokeWidth = 2.5.dp,
-                        )
-                        Spacer(modifier = Modifier.height(36.dp))
                     }
                 }
+            ) {
+                Box(modifier = Modifier.fillMaxSize()) {
 
-                if (state.scanQRCodeResult is Resource.Loading) {
-                    AndroidView(
-                        { context ->
-                            val cameraExecutor = Executors.newSingleThreadExecutor()
-                            val previewView = PreviewView(context).also {
-                                it.scaleType = PreviewView.ScaleType.FILL_CENTER
+                    val scanQRResult = state.scanQRCodeResult
+                    val uploadWcUriResult = state.uploadWcUri
+
+                    if (uploadWcUriResult is Resource.Success) {
+                        ScanQRBoxUI {
+                            Spacer(modifier = Modifier.height(36.dp))
+                            Text(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                text = "DApp Connected Successfully!",
+                                textAlign = TextAlign.Center,
+                                color = TextBlack,
+                                fontSize = 22.sp
+                            )
+                            Spacer(modifier = Modifier.height(36.dp))
+                            CensoButton(onClick = viewModel::userFinished) {
+                                Text(
+                                    modifier = Modifier.padding(8.dp),
+                                    text = "Done",
+                                    fontSize = 20.sp,
+                                    color = CensoWhite
+                                )
                             }
-                            val cameraProviderFuture =
-                                ProcessCameraProvider.getInstance(context)
-                            cameraProviderFuture.addListener({
-                                val cameraProvider: ProcessCameraProvider =
-                                    cameraProviderFuture.get()
+                            Spacer(modifier = Modifier.height(36.dp))
+                        }
+                    } else if (scanQRResult is Resource.Error || uploadWcUriResult is Resource.Error) {
+                        ScanQRBoxUI {
+                            Spacer(modifier = Modifier.height(36.dp))
+                            Text(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                text = if (scanQRResult is Resource.Error) "Failed to scan QR Code" else "Failed to upload code to backend",
+                                textAlign = TextAlign.Center,
+                                color = TextBlack,
+                                fontSize = 22.sp
+                            )
+                            Spacer(modifier = Modifier.height(36.dp))
+                            CensoButton(onClick = viewModel::retryScan) {
+                                Text(
+                                    modifier = Modifier.padding(8.dp),
+                                    text = "Try Again",
+                                    fontSize = 20.sp,
+                                    color = CensoWhite
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(36.dp))
+                        }
+                    } else if (scanQRResult is Resource.Uninitialized) {
+                        ScanQRBoxUI {
+                            Spacer(modifier = Modifier.height(36.dp))
+                            Text(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                text = "Start Scanning...",
+                                textAlign = TextAlign.Center,
+                                color = TextBlack,
+                                fontSize = 22.sp
+                            )
+                            Spacer(modifier = Modifier.height(36.dp))
+                            CensoButton(onClick = viewModel::startScanning) {
+                                Text(
+                                    modifier = Modifier.padding(8.dp),
+                                    text = "Scan QR Code",
+                                    fontSize = 20.sp,
+                                    color = CensoWhite
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(36.dp))
+                        }
+                    } else if (scanQRResult is Resource.Success) {
+                        ScanQRBoxUI {
+                            Spacer(modifier = Modifier.height(36.dp))
+                            Text(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                text = "Found wallet, uploading data...",
+                                textAlign = TextAlign.Center,
+                                color = TextBlack,
+                                fontSize = 22.sp
+                            )
+                            Spacer(modifier = Modifier.height(36.dp))
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(20.dp),
+                                color = ButtonRed,
+                                strokeWidth = 2.5.dp,
+                            )
+                            Spacer(modifier = Modifier.height(36.dp))
+                        }
+                    }
 
-                                val preview = Preview.Builder()
-                                    .build()
-                                    .also {
-                                        it.setSurfaceProvider(previewView.surfaceProvider)
-                                    }
-
-                                val imageCapture = ImageCapture.Builder().build()
-
-                                val imageAnalyzer = ImageAnalysis.Builder()
-                                    .build()
-                                    .also {
-                                        it.setAnalyzer(
-                                            cameraExecutor,
-                                            WalletConnectBarcodeAnalyzer { uri ->
-                                                viewModel.receivedWalletConnectUri(uri ?: "")
-                                            })
-                                    }
-
-                                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-                                try {
-                                    // Unbind use cases before rebinding
-                                    cameraProvider.unbindAll()
-
-                                    // Bind use cases to camera
-                                    cameraProvider.bindToLifecycle(
-                                        context as ComponentActivity,
-                                        cameraSelector,
-                                        preview,
-                                        imageCapture,
-                                        imageAnalyzer
-                                    )
-
-                                } catch (exc: Exception) {
-                                    exc.printStackTrace()
-                                    censoLog(message = "Use case binding failed: $exc")
+                    if (state.scanQRCodeResult is Resource.Loading) {
+                        AndroidView(
+                            { context ->
+                                val cameraExecutor = Executors.newSingleThreadExecutor()
+                                val previewView = PreviewView(context).also {
+                                    it.scaleType = PreviewView.ScaleType.FILL_CENTER
                                 }
-                            }, ContextCompat.getMainExecutor(context))
-                            previewView
-                        },
-                        modifier = Modifier.fillMaxSize()
-                    )
+                                val cameraProviderFuture =
+                                    ProcessCameraProvider.getInstance(context)
+                                cameraProviderFuture.addListener({
+                                    val cameraProvider: ProcessCameraProvider =
+                                        cameraProviderFuture.get()
+
+                                    val preview = Preview.Builder()
+                                        .build()
+                                        .also {
+                                            it.setSurfaceProvider(previewView.surfaceProvider)
+                                        }
+
+                                    val imageCapture = ImageCapture.Builder().build()
+
+                                    val imageAnalyzer = ImageAnalysis.Builder()
+                                        .build()
+                                        .also {
+                                            it.setAnalyzer(
+                                                cameraExecutor,
+                                                WalletConnectBarcodeAnalyzer { uri ->
+                                                    viewModel.receivedWalletConnectUri(uri ?: "")
+                                                })
+                                        }
+
+                                    val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                                    try {
+                                        // Unbind use cases before rebinding
+                                        cameraProvider.unbindAll()
+
+                                        // Bind use cases to camera
+                                        cameraProvider.bindToLifecycle(
+                                            context as ComponentActivity,
+                                            cameraSelector,
+                                            preview,
+                                            imageCapture,
+                                            imageAnalyzer
+                                        )
+
+                                    } catch (exc: Exception) {
+                                        exc.printStackTrace()
+                                        censoLog(message = "Use case binding failed: $exc")
+                                    }
+                                }, ContextCompat.getMainExecutor(context))
+                                previewView
+                            },
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
                 }
             }
         }
