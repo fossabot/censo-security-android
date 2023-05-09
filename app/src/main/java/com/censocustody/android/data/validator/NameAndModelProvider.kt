61b04ed4c0caf0ab98c13ed062a23259c198a1de
SamIAmHarris <sam@groundcrewdev.com>
2023-05-09 17:20:45 -0400

    Report crash if we fail to get device name or model

--- app/src/main/java/com/censocustody/android/data/validator/NameAndModelProvider.kt
+++ app/src/main/java/com/censocustody/android/data/validator/NameAndModelProvider.kt
@@ -3,6 +3,8 @@ package com.censocustody.android.data.validator
 import android.content.Context
 import android.os.Build
 import android.provider.Settings
+import com.censocustody.android.common.util.CrashReportingUtil
+import com.censocustody.android.common.util.sendError
 import com.censocustody.android.data.models.NameAndModel
 
 interface NameAndModelProvider {
@@ -13,9 +15,17 @@ class AndroidNameAndModelProvider(private val applicationContext: Context) :
     NameAndModelProvider {
 
     override fun retrieveNameAndModel(): NameAndModel {
-        return NameAndModel(
-            name = Settings.Global.getString(applicationContext.contentResolver, Settings.Global.DEVICE_NAME),
-            model = "Android ${Build.MANUFACTURER} - ${Build.DEVICE} (${Build.MODEL})"
-        )
+        return try {
+            NameAndModel(
+                name = Settings.Global.getString(
+                    applicationContext.contentResolver,
+                    Settings.Global.DEVICE_NAME
+                ),
+                model = "${Build.MANUFACTURER} - ${Build.DEVICE} (${Build.MODEL})"
+            )
+        } catch (e: Exception) {
+            e.sendError(CrashReportingUtil.RETRIEVE_NAME_MODEL)
+            NameAndModel("", "")
+        }
     }
 }
