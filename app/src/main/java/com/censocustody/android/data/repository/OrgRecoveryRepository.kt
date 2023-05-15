d3eecbce64673f46699eb903f2703575935207a6
bflood-strike <42786522+bflood-strike@users.noreply.github.com>
2023-05-15 10:39:56 -0400

    ore recovery model updates to include policy and addresses who already signed in my-org-recovery-request. (#489)

--- app/src/main/java/com/censocustody/android/data/repository/OrgRecoveryRepository.kt
+++ app/src/main/java/com/censocustody/android/data/repository/OrgRecoveryRepository.kt
@@ -2,13 +2,12 @@ package com.censocustody.android.data.repository
 
 import com.censocustody.android.common.Resource
 import com.censocustody.android.data.api.BrooklynApiService
-import com.censocustody.android.data.models.*
-import com.censocustody.android.data.models.recovery.OrgAdminRecoveryRequest
+import com.censocustody.android.data.models.recovery.OrgAdminRecoveryRequestEnvelope
 import com.censocustody.android.data.models.recovery.OrgAdminRecoverySignaturesRequest
 import javax.inject.Inject
 
 interface OrgRecoveryRepository {
-    suspend fun getMyOrgAdminRecoveryRequest(): Resource<OrgAdminRecoveryRequest>
+    suspend fun getMyOrgAdminRecoveryRequest(): Resource<OrgAdminRecoveryRequestEnvelope>
     suspend fun registerOrgAdminRecoverySignatures(
         recoveryAddress: String,
         signatures: List<OrgAdminRecoverySignaturesRequest.Signature>,
@@ -19,7 +18,7 @@ class OrgRecoveryRepositoryImpl @Inject constructor(
     private val api: BrooklynApiService,
 ) : OrgRecoveryRepository, BaseRepository() {
 
-    override suspend fun getMyOrgAdminRecoveryRequest(): Resource<OrgAdminRecoveryRequest> =
+    override suspend fun getMyOrgAdminRecoveryRequest(): Resource<OrgAdminRecoveryRequestEnvelope> =
         retrieveApiResource { api.getMyOrgAdminRecoveryRequest() }
 
     override suspend fun registerOrgAdminRecoverySignatures(
