b12317b7750060fc232f34fe697ecc2c73dbd1ad
B Flood <bflood@strikeprotocols.com>
2023-05-24 18:06:19 -0400

    wallet name update - some fixes for fees and from/to changes

--- app/src/main/java/com/censocustody/android/presentation/approval_detail/approval_type_detail_items/NameUpdateDetailContent.kt
+++ app/src/main/java/com/censocustody/android/presentation/approval_detail/approval_type_detail_items/NameUpdateDetailContent.kt
@@ -6,25 +6,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.censocustody.android.R
 import com.censocustody.android.data.models.approvalV2.ApprovalRequestDetailsV2
 import com.censocustody.android.presentation.approvals.ApprovalContentHeader
 import com.censocustody.android.presentation.approvals.approval_type_row_items.RenameType
-import com.censocustody.android.presentation.approvals.approval_type_row_items.buildFromToDisplayText
 import com.censocustody.android.presentation.approvals.approval_type_row_items.formattedUsdEquivalentWithSymbol
+import com.censocustody.android.presentation.components.CensoTagRow
+import com.censocustody.android.presentation.components.FactRow
 import com.censocustody.android.presentation.components.FactsData
 import com.censocustody.android.presentation.components.RowData
 
 @Composable
 fun NameUpdateDetailContent(header: String, oldName: String, newName: String, renameType: RenameType,
                             chainFees: List<ApprovalRequestDetailsV2.ChainFee>? = null) {
-    val fromToText = buildFromToDisplayText(from = oldName, to = newName, LocalContext.current, renameType)
 
     ApprovalContentHeader(header = header, topSpacing = 24, bottomSpacing = 36)
-    ApprovalSubtitle(text = fromToText, fontSize = 20.sp)
+    CensoTagRow(text1 = oldName, text2 = newName, arrowForward = true)
     if (chainFees != null && chainFees.isNotEmpty()) {
-        FactsData(
+        Spacer(modifier = Modifier.height(28.dp))
+        FactRow(factsData = FactsData(
             title = LocalContext.current.getString(R.string.fees),
             facts = chainFees.map {
                 RowData.KeyValueRow(
@@ -32,7 +32,7 @@ fun NameUpdateDetailContent(header: String, oldName: String, newName: String, re
                     value = it.fee.formattedUsdEquivalentWithSymbol()
                 )
             }
-        )
+        ))
     }
     Spacer(modifier = Modifier.height(28.dp))
 }
