fa3a9379bd5b7fdf7131007085cd5d4e0ab36435
B Flood <bflood@strikeprotocols.com>
2023-05-24 18:06:19 -0400

    wallet name update - some fixes for fees and from/to changes

--- app/src/main/java/com/censocustody/android/presentation/approvals/approval_type_row_items/NameUpdateRowContent.kt
+++ app/src/main/java/com/censocustody/android/presentation/approvals/approval_type_row_items/NameUpdateRowContent.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.censocustody.android.presentation.approval_detail.approval_type_detail_items.ApprovalSubtitle
 import com.censocustody.android.presentation.approvals.ApprovalContentHeader
+import com.censocustody.android.presentation.components.CensoTagRow
 
 @Composable
 fun NameUpdateRowContent(
@@ -17,9 +15,8 @@ fun NameUpdateRowContent(
     newName: String,
     renameType: RenameType
 ) {
-    val fromToText = buildFromToDisplayText(from = oldName, to = newName, LocalContext.current, renameType)
 
     ApprovalContentHeader(header = header, topSpacing = 16, bottomSpacing = 8)
-    ApprovalSubtitle(text = fromToText, fontSize = 20.sp)
+    CensoTagRow(text1 = oldName, text2 = newName, arrowForward = true)
     Spacer(modifier = Modifier.height(20.dp))
 }
