521febb5b3009eba377fe61cee8546413bf3e8ae
SamIAmHarris <sam@groundcrewdev.com>
2023-05-26 12:32:29 -0400

    Initial UI for selecting wallet

--- app/src/main/java/com/censocustody/android/data/models/WalletConnectPairingRequest.kt
+++ app/src/main/java/com/censocustody/android/data/models/WalletConnectPairingRequest.kt
@@ -1,5 +1,5 @@
 package com.censocustody.android.data.models
 
-data class WalletConnectPairingRequest(val uri: String)
+data class WalletConnectPairingRequest(val uri: String, val walletAddresses: List<String>? = null)
 
 data class WalletConnectPairingResponse(val topic: String)
