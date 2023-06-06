a80830865537f5296a71e154f857554320ec6bf3
Benjamin Holzman <bholzman@censo.co>
2023-06-05 23:04:10 -0400

    handle arrays in EIP712 data

--- app/src/main/java/com/censocustody/android/presentation/approval_detail/approval_type_detail_items/DAppEthSignTypedDataDetailContent.kt
+++ app/src/main/java/com/censocustody/android/presentation/approval_detail/approval_type_detail_items/DAppEthSignTypedDataDetailContent.kt
@@ -21,6 +21,7 @@ import com.censocustody.android.presentation.components.FactsData
 import com.censocustody.android.presentation.components.RowData
 import com.censocustody.android.ui.theme.BackgroundLight
 import com.censocustody.android.ui.theme.DarkGreyText
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 
 @Composable
@@ -89,20 +90,34 @@ fun EIP712Entry(eip712Data: EIP712Data, entry: EIP712Data.Entry) {
             textAlign = TextAlign.End
         )
         Text(
-            text = if (eip712Data.hasType(entry.type)) { entry.type } else { entry.value.toString().trim('"') },
+            text = if (eip712Data.hasType(entry.baseType)) { entry.type } else { entry.value.toString().trim('"') },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
             textAlign = TextAlign.Start
         )
     }
-    if (eip712Data.hasType(entry.type)) {
+    if (eip712Data.hasType(entry.baseType)) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(
             PaddingValues(start = 16.dp)
         )) {
             Column {
-                for (subEntry in eip712Data.getEntriesForType(entry.value as JsonObject, entry.type)) {
-                    EIP712Entry(eip712Data = eip712Data, entry = subEntry)
+                if (entry.isArray) {
+                    for (value in entry.value as JsonArray) {
+                        for (subEntry in eip712Data.getEntriesForType(
+                            value as JsonObject,
+                            entry.baseType
+                        )) {
+                            EIP712Entry(eip712Data = eip712Data, entry = subEntry)
+                        }
+                    }
+                } else {
+                    for (subEntry in eip712Data.getEntriesForType(
+                        entry.value as JsonObject,
+                        entry.type
+                    )) {
+                        EIP712Entry(eip712Data = eip712Data, entry = subEntry)
+                    }
                 }
             }
         }
@@ -166,6 +181,20 @@ fun DAppEthSignTypedDataDetailContentPreview() {
         dAppInfo = ApprovalRequestDetailsV2.DAppInfo("dApp Name", "dApp.url", "dApp Description", emptyList()),
         data = ApprovalRequestDetailsV2.DAppParams.EthSignTypedData(
             "{\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallet\",\"type\":\"address\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person\"},{\"name\":\"contents\",\"type\":\"string\"}]},\"primaryType\":\"Mail\",\"domain\":{\"name\":\"Ether Mail\",\"version\":\"1\",\"chainId\":1,\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\"},\"message\":{\"from\":{\"name\":\"Cow\",\"wallet\":\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\"},\"to\":{\"name\":\"Bob\",\"wallet\":\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\"},\"contents\":\"Hello, Bob!\"}}", ""
+        ).eip712Data()
+    )
+}
+
+@Composable
+@Preview
+fun DAppEthSignTypedDataArrayDetailContentPreview() {
+    DAppEthSignTypedDataDetailContent(
+        header = "DApp Transaction",
+        fromAccount = "from wallet",
+        fee = ApprovalRequestDetailsV2.Amount("0.00123", "0.0012300", "0.24"),
+        dAppInfo = ApprovalRequestDetailsV2.DAppInfo("dApp Name", "dApp.url", "dApp Description", emptyList()),
+        data = ApprovalRequestDetailsV2.DAppParams.EthSignTypedData(
+            "{\"types\":{\"MetaTransactionDataV2\":[{\"name\":\"signer\",\"type\":\"address\"},{\"name\":\"sender\",\"type\":\"address\"},{\"name\":\"expirationTimeSeconds\",\"type\":\"uint256\"},{\"name\":\"salt\",\"type\":\"uint256\"},{\"name\":\"callData\",\"type\":\"bytes\"},{\"name\":\"feeToken\",\"type\":\"address\"},{\"name\":\"fees\",\"type\":\"MetaTransactionFeeData[]\"}],\"MetaTransactionFeeData\":[{\"name\":\"recipient\",\"type\":\"address\"},{\"name\":\"amount\",\"type\":\"uint256\"}],\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}]},\"domain\":{\"name\":\"ZeroEx\",\"version\":\"1.0.0\",\"chainId\":\"1\",\"verifyingContract\":\"0xdef1c0ded9bec7f1a1670819833240f027b25eff\"},\"primaryType\":\"MetaTransactionDataV2\",\"message\":{\"signer\":\"0xa8120f2ca4b3495d460df02122e90833e928da86\",\"sender\":\"0x0000000000000000000000000000000000000000\",\"expirationTimeSeconds\":\"1685998373\",\"salt\":\"10172818397776878889035290569885733773193256001012696978400198892263007391591\",\"callData\":\"0x7a1eb1b9000000000000000000000000b50721bcf8d\",\"feeToken\":\"0xb50721bcf8d664c30412cfbc6cf7a15145234ad1\",\"fees\":[{\"recipient\":\"0x38f5e5b4da37531a6e85161e337e0238bb27aa90\",\"amount\":\"17280505867230653594\"}]}}", ""
         ).eip712Data()
     )
 }
